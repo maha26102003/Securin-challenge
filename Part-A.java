d91264ef944c5d885bb32c228b6d6156e71e93bc
maha26102003 <150503893+maha26102003@users.noreply.github.com>
2024-04-05 09:32:34 -0700

    Update Part-A.java

--- Part-A.java
+++ Part-A.java
@@ -7,25 +7,25 @@ public class Main
 		int[] b={1,2,3,4,5,6};
 		
 		//1.Total Combinations possible
-		
+	
 		int tot_comb=a.length*b.length;
 		System.out.println("Total Combinations possible in a dice game is:"+tot_comb);
-
 		
 		//2.Distribution of all possible Combination
+		
 		HashMap<Integer,Integer> h=new HashMap<>();
 		System.out.print("Distribution of all possible combination:\n");
 		for(int i=0;i<6;i++){
 		    for(int j=0;j<6;j++){
 		        int t=a[i]+b[j];
-		        System.out.print("("+a[i]+","+b[j]+")");
+		        System.out.print("("+a[i]+","+b[j]+")="+t+" ");
 		        h.put(t,h.getOrDefault(t,0)+1);
 		    }
 		    System.out.println();
 		}
-
 		
 		//3.probability of all possible sums
+		
 		System.out.println("Probability of all possible sums:");
 		for(int i=2;i<=12;i++){
 		    double p=(double)h.get(i)/tot_comb;
@@ -35,17 +35,18 @@ public class Main
 	}
 }
 
+
 /* 
 Output:
 
 Total Combinations possible in a dice game is:36
 Distribution of all possible combination:
-(1,1)(1,2)(1,3)(1,4)(1,5)(1,6)
-(2,1)(2,2)(2,3)(2,4)(2,5)(2,6)
-(3,1)(3,2)(3,3)(3,4)(3,5)(3,6)
-(4,1)(4,2)(4,3)(4,4)(4,5)(4,6)
-(5,1)(5,2)(5,3)(5,4)(5,5)(5,6)
-(6,1)(6,2)(6,3)(6,4)(6,5)(6,6)
+(1,1)=2 (1,2)=3 (1,3)=4 (1,4)=5 (1,5)=6 (1,6)=7 
+(2,1)=3 (2,2)=4 (2,3)=5 (2,4)=6 (2,5)=7 (2,6)=8 
+(3,1)=4 (3,2)=5 (3,3)=6 (3,4)=7 (3,5)=8 (3,6)=9 
+(4,1)=5 (4,2)=6 (4,3)=7 (4,4)=8 (4,5)=9 (4,6)=10 
+(5,1)=6 (5,2)=7 (5,3)=8 (5,4)=9 (5,5)=10 (5,6)=11 
+(6,1)=7 (6,2)=8 (6,3)=9 (6,4)=10 (6,5)=11 (6,6)=12 
 Probability of all possible sums:
 P(2) = 1/36 = 0.0278
 P(3) = 2/36 = 0.0556
@@ -58,4 +59,5 @@ P(9) = 4/36 = 0.1111
 P(10) = 3/36 = 0.0833
 P(11) = 2/36 = 0.0556
 P(12) = 1/36 = 0.0278
+
 */
